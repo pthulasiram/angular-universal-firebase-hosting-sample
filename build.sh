ccae36d22047e8a7264244b3e540f68d19c427bf
David East <deast@google.com>
2017-08-09 13:49:41 -0600

    fix break

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ mv dist-server/dist/main.* dist-server/dist/main.bundle.js
 mv dist-server/main.* dist-server/main.bundle.js
 sed -i '' 's/main.*/main.bundle.js\">\<\/script>\<\/body>\<\/html>\ /g' dist-server/index.html
 cp server/package.json dist-server
-cd dist-server 
-npm i
-cd ..
+# cd dist-server 
+# npm i
+# cd ..
--- server/index.ts
+++ server/index.ts
@@ -1,50 +1,8 @@
-import * as express from 'express';
-import { enableProdMode as enableProd } from '@angular/core';
-import { renderModuleFactory } from '@angular/platform-server';
-import * as fs from 'fs';
-import { Observable, Observer } from 'rxjs';
-import 'zone.js/dist/zone-node';
+import * as angularUniversal from 'angular-universal-express-firebase';
 
-export interface ServerConfiguration {
-  main: string;
-  index: string;
-  enableProdMode?: boolean;
-  staticDirectory?: string;
-}
-
-function readFile$(file: string): Observable<string> {
-  return Observable.create((observer: Observer<string>) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if(err) { 
-        observer.error(err); 
-      } else { 
-        observer.next(data);
-        observer.complete();
-      }
-    });
-  });
-}
-
-function angularUniversal({ index, main, staticDirectory, enableProdMode = false }: ServerConfiguration) {
-  if (enableProdMode) { enableProd(); }
-  return (req: express.Request, res: express.Response) => {
-    readFile$(index)
-      .mergeMap(document => {
-        const url = req.path;
-        const AppServerModuleNgFactory = require(main).AppServerModuleNgFactory;
-        return Observable.from(renderModuleFactory(AppServerModuleNgFactory, { document, url }))
-      })
-      .take(1)
-      .subscribe(html => { res.send(html); });
-  };
-}
-
-const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', angularUniversal({
+export let trigger = angularUniversal.trigger({
   index: __dirname + '/index.html',
   main: __dirname + '/main.bundle',
+  staticDirectory: __dirname + '/dist',
   enableProdMode: true
-}));
-
-app.listen('3005', () => { console.log('Listening on 3005...'); });
+});

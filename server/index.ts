e1620a2d74bc2f873d3b9ffa9737cac1c5ae8094
Kiana McNellis <kiana@kmcnellis.com>
2017-12-06 17:11:10 -0800

    Basic fixes & updates for running on alternate systems

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,10 @@
 import * as angularUniversal from 'angular-universal-express-firebase';
 
 export let trigger = angularUniversal.trigger({
-  index: __dirname + '/index.html',
-  main: __dirname + '/main.bundle',
+  index: __dirname + '/dist-server/index.html',
+  main: __dirname + '/dist-server /main.bundle',
   staticDirectory: __dirname + '/dist',
-  enableProdMode: true
+  enableProdMode: true,
+  cdnCacheExpiry: 1200,
+  browserCacheExpiry: 600
 });
